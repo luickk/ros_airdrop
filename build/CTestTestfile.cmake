699c38596d676e149104060fd963db3964f00374
MrGrimod <gelberdev@gmail.com>
2018-10-08 19:01:51 +0000

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/airdrop_ros/src
+# Build directory: /home/pi/airdrop_ros/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
